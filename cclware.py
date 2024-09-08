abe294eb7f16b0939f726b853a042083f281ebbc
Daniel Sedoff <danielsedoff@gmail.com>
2024-09-09 01:04:46 +0300

    first use of pylint

--- cclware.py
+++ cclware.py
@@ -1,30 +1,36 @@
+""" cclware main module. see danielsedoff@github """
+
 from datetime import datetime
-from history import history 
+from history import history
 
 
-newline = "\n"
+NEWLINE = "\n"
+LOG_FILE = 'ccl.log'
+
 commands = []
 commands.append(("history", history))
 
 def extract_params(line, cmd):
+    """ extract parameters from the user input string """
     return line[len(cmd):]
 
 def time_now():
+    """ current date and time as string formatted together with prompt symbol """
     now = datetime.now()
     dt = now.strftime("%d/%m/%Y %H:%M:%S > ")
     return dt
 
 def log(line):
-    with open('ccl.log', 'a') as log_file:
-        log_file.write(time_now() + line + newline)
-    
-while(True):
+    """ append a line to the log file """
+    with open(LOG_FILE, 'a', encoding='utf8') as log_file:
+        log_file.write(time_now() + line + NEWLINE)
+
+while True:
     userLine = input("> ").strip()
     log(userLine)
     userCmd = userLine.split(" ")[0]
     print ("cmd is: " + userCmd)
     for command in commands:
-        if(command[0] == userCmd):
+        if command[0] == userCmd:
             commandFunction = command[1]
             commandFunction(extract_params(userLine, userCmd))
-    
--- history.py
+++ history.py
@@ -1,6 +1,10 @@
-newline = "\n"
+""" This module contains a History command that takes its commandline args as a whole string. """
+
+LOG_FILE = 'ccl.log'
+
 
 def history(line):
-    with open('ccl.log') as log_file:
+    """ The History command. """
+    with open(LOG_FILE, encoding='utf8') as log_file:
         lines = list(filter(lambda l: line in l, log_file.readlines()))
         print("".join(lines))
